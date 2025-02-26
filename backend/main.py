3b9c0005a720e99a0668aaf4bf26ec9715536a32
Cleiver Júnior <jr.cleiver@gmail.com>
2025-02-26 12:27:06 -0300

    Mudando porta do uvicorn

--- backend/main.py
+++ backend/main.py
@@ -27,4 +27,4 @@ async def secure_endpoint(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
